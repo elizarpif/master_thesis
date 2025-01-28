e42d698168eb47858c72b191be55f1ad3d3efd32
elizarpif <elizarpif@gmail.com>
2025-01-28 21:52:01 +0100

    added downsampling

---
+++ downsampleRosslerSignal.m
@@ -0,0 +1,23 @@
+function [x, t] = downsampleRosslerSignal(x_res, t, isPlotDynamics)
+% in order to have approx 20 samples per cycle, downsample
+ds_factor = 9;
+
+% Downsample the x signal using the calculated rate
+x_downsampled = x_res(1:ds_factor:end);
+t_downsampled = t(1:ds_factor:end);
+
+% and take only 4096 last samples
+number_of_samples = 4096;
+x = x_downsampled(end-number_of_samples+1: end);
+t = t_downsampled(end-number_of_samples+1: end);
+
+% Optional: Plot the downsampled signal
+if isPlotDynamics
+    figure;
+    plot(t, x);
+    title('Downsampled Signal');
+    xlabel('Time');
+    ylabel('Values');
+    grid on;
+end
+end

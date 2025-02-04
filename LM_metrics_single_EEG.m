c95dbac1e386a3ff26cebe3ff535a28d29a6a9be
elizarpif <elizarpif@gmail.com>
2025-02-05 00:35:26 +0100

    saved state of new version

--- LM_metrics_single_EEG.m
+++ LM_metrics_single_EEG.m
@@ -1,5 +1,5 @@
 % Load data from file
-data = load("bern dataset/100 seizures/Pat15/P15_Sz1_block37.mat");
+data = load("bern dataset/100 seizures/Pat16/P16_Sz1_block37.mat");
 eegDataOriginal = data.EEG';
 
 % Define sampling frequency and time vector
@@ -12,8 +12,59 @@ time_vector = 0:Ts:total_duration; % Time vector
 channelNameArray = data.channelNameArray;
 numChannels = length(channelNameArray); % Number of EEG channels
 
+
+% Anti-aliasing filter
+newFs = Fs/2;
+newNyquistFreq = newFs/2;
+lowpassCutoff = newNyquistFreq-10; % low pass is slightly below the new Nyquist
+order = 8; % selected this order as an example
+% Создание полосового фильтра Баттерворта для поиска спайков
+[b, a] = butter(order, lowpassCutoff / newNyquistFreq, 'low');
+%
+% exampleChannelData = eegData(1, :);
+% filteredExampleChannelData = filter(b, a, exampleChannelData);
+%
+% figure();
+% [p, f] = periodogram(exampleChannelData);
+% plot(f * Fs / (2*pi), 10*log10(p));
+% title('TBR05');
+% xlabel('Frequency (Hz)')
+% ylabel('Power/frequency (dB/(rad/sample))');
+% hold on;
+% yline(0, '--r');
+% xline(newNyquistFreq, '--r');
+% hold off;
+%
+% %verification
+% figure();
+% [p, f] = periodogram(filteredExampleChannelData);
+% plot(f * Fs / (2*pi), 10*log10(p));
+% title('TPL06\_filtered');
+% xlabel('Frequency (Hz)');
+% ylabel('Power/frequency (dB/(rad/sample))');
+% hold on;
+% yline(0, '--r');
+% xline(newNyquistFreq, '--r');
+% hold off;
+
+% Ensure even number of samples for downsampling
+if mod(size(eegData, 2), 2) ~= 0
+    eegData = eegData(:, 1:end-1); % Trim to make length even
+end
+
+% Downsample data
+downsampledEEGData = zeros(numChannels, size(eegDataOriginal, 2) / 2);
+for i = 1:numChannels
+    channelData = eegDataOriginal(i, :);
+    filteredChannelData = filter(b, a, channelData);
+
+    downsampledData = downsample(filteredChannelData, 2);
+    downsampledEEGData(i, :) = downsampledData;
+end
+
+
 % Set interval jump for processing
-intervalJump = 10240; % Number of samples to jump for each interval
+intervalJump = 10240 / 2; % Number of samples to jump for each interval
 totalIntervals = floor(length(eegDataOriginal) / intervalJump);
 
 % Initialize overall storage for metrics
@@ -22,10 +73,10 @@ L_XY_all = zeros(numPairs, totalIntervals);
 L_YX_all = zeros(numPairs, totalIntervals);
 R_all = zeros(numPairs, totalIntervals);
 
+
 % Process each interval of EEG data
-intervalIndex = 0; % Initialize interval index
-for interval = 1:intervalJump:length(eegDataOriginal)
-    if interval + intervalJump > length(eegDataOriginal)
+for interval = intervalJump:intervalJump:length(downsampledEEGData)
+    if interval + intervalJump > length(downsampledEEGData)
         disp('Break: Interval exceeds data length')
         break;
     end
@@ -35,59 +86,8 @@ for interval = 1:intervalJump:length(eegDataOriginal)
 
     % Calculate indices for the current slice of data
     l = interval;
-    r = min(interval + intervalJump, length(eegDataOriginal));  % Ensure r does not exceed data length
+    r = min(interval + intervalJump, length(downsampledEEGData));  % Ensure r does not exceed data length
 
-    % Extract data for current interval
-    eegData = eegDataOriginal(:, l:r);
-
-    % Ensure even number of samples for downsampling
-    if mod(size(eegData, 2), 2) ~= 0
-        eegData = eegData(:, 1:end-1); % Trim to make length even
-    end
-
-    % Anti-aliasing filter
-    newFs = Fs/2;
-    newNyquistFreq = newFs/2;
-    lowpassCutoff = newNyquistFreq-10; % low pass is slightly below the new Nyquist
-    order = 8; % selected this order as an example
-    % Создание полосового фильтра Баттерворта для поиска спайков
-    [b, a] = butter(order, lowpassCutoff / newNyquistFreq, 'low');
-    % 
-    % exampleChannelData = eegData(1, :);
-    % filteredExampleChannelData = filter(b, a, exampleChannelData);
-    % 
-    % figure();
-    % [p, f] = periodogram(exampleChannelData);
-    % plot(f * Fs / (2*pi), 10*log10(p));
-    % title('TBR05');
-    % xlabel('Frequency (Hz)')
-    % ylabel('Power/frequency (dB/(rad/sample))');
-    % hold on;
-    % yline(0, '--r');
-    % xline(newNyquistFreq, '--r');
-    % hold off;
-    % 
-    % %verification
-    % figure();
-    % [p, f] = periodogram(filteredExampleChannelData);
-    % plot(f * Fs / (2*pi), 10*log10(p));
-    % title('TPL06\_filtered');
-    % xlabel('Frequency (Hz)');
-    % ylabel('Power/frequency (dB/(rad/sample))');
-    % hold on;
-    % yline(0, '--r');
-    % xline(newNyquistFreq, '--r');
-    % hold off;
-
-    % Downsample data
-    cutEEGData = zeros(numChannels, size(eegData, 2) / 2);
-    for i = 1:numChannels
-        channelData = eegData(i, :);
-        filteredChannelData = filter(b, a, channelData);
-
-        downsampledData = downsample(filteredChannelData, 2);
-        cutEEGData(i, :) = downsampledData;
-    end
 
     % Create pairs of EEG channels
     selectedPairs = cell(1, numPairs);
@@ -95,7 +95,7 @@ for interval = 1:intervalJump:length(eegDataOriginal)
 
     for idx = 1:numPairs
         eegIdx = 2 * idx - 1;
-        pair = cutEEGData(eegIdx:eegIdx + 1, :);
+        pair = downsampledEEGData(eegIdx:eegIdx + 1, l:r);
         selectedPairs{idx} = pair;
         selectedPairNames(idx) = sprintf('%s-%s', channelNameArray{eegIdx}, channelNameArray{eegIdx + 1});
     end
@@ -110,15 +110,14 @@ for interval = 1:intervalJump:length(eegDataOriginal)
         % Compute R metric
         R_all(idx, intervalIndex) = computeRMetric(selectedPairs{idx}');
     end
-
     % Log processing completion
-    logger(sprintf("Processing completed for %d intervals, interval = %d", interval, interval));
+    logger(sprintf("Processing completed for interval = %d", interval));
 end
 
-timePoints = (intervalJump:intervalJump:length(eegDataOriginal)-1) / Fs;
+timePoints = (intervalJump:intervalJump:length(downsampledEEGData)-1) / newFs;
 
-eegImagescResult(timePoints, L_XY_all, numPairs, intervalJump, Fs, 'L_{XY}');
-eegImagescResult(timePoints, L_YX_all, numPairs, intervalJump, Fs, 'L_{YX}');
-eegImagescResult(timePoints, R_all, numPairs, intervalJump, Fs, 'R');
-eegImagescResult(timePoints, L_XY_all - L_YX_all, numPairs, intervalJump, Fs, 'delta L');
-
+% eegImagescResult(timePoints, L_XY_all, numPairs, intervalJump, Fs, 'L_{XY}');
+% eegImagescResult(timePoints, L_YX_all, numPairs, intervalJump, Fs, 'L_{YX}');
+% eegImagescResult(timePoints, R_all, numPairs, intervalJump, Fs, 'R');
+eegImagescResult(timePoints, abs(L_XY_all - L_YX_all), numPairs, intervalJump, newFs, 'delta L');
+%
